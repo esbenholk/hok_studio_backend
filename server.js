1a3587e94a296ac97fb9bd89eb07aa1625d6b029
esben holk nielsen <esbenholknielsen@gmail.com>
2021-05-12 19:31:33 +0200

    m

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const {cloudinary} = require('./utils/cloudinary')
+
+const port = process.env.PORT || 3001
+
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit:'50mb', extended: true}))
+
+
+
+app.get('/api/getallimages', async (req, res)=>{
+    const {resources}= await cloudinary.search.expression(
+        'folder:contentRedistribution' // add your folder
+        ).sort_by('public_id','desc').max_results(99).execute()
+
+    const publicUrls = resources.map((file)=> file.url);
+
+    res.send(publicUrls);
+
+})
+app.post('/api/upload', async (req, res)=>{
+    try {
+        const fileStr = req.body.data;
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {upload_preset:'contentRedistribution'})
+        console.log("there is sucess", uploadedResponse);
+        res.json({msg: "succes"});
+    } catch (error) {
+        console.error("there is error", error)
+        res.status(500).json({err: 'there is an error'})
+    }
+})
+
+
+
+app.listen(port, ()=>{
+    console.log('i am listening on', port);
+
+})
+
+
